d3e1d06840e3bcc8cbc843d777d85c50ac4b866b
Shamshad Akther <shamshad.cse90@gmail.com>
2021-02-17 13:34:58 +0200

    Add files via upload

--- attributeClusteringBlocking.py
+++ attributeClusteringBlocking.py
@@ -18,7 +18,7 @@ clusterLinks1 = {}
 clusterLinks2 = {}
 blocks = dict()
 
-# create a list of attribute names, annotated with the occuring values
+# create a list of attribute names, with the occuring values
 def extractAttributeNames(entitiesList, datasetIndex):
     # unique set of attribute Names
     attributeNames = set()
@@ -37,11 +37,14 @@ def extractAttributeNames(entitiesList, datasetIndex):
                 attributeNameDict[(attributeName, datasetIndex)].append(token)
     return attributeNameDict
 
+#find the jaccard similarity between two set of attributes names values
+#|token(attributenames1) ∩ token(attributenames2)| / |token(attributenames1) ∪ token(attributenames2)|
 def jaccardSimilarity(valueList1, valueList2):
     commontokens = list(set(valueList1).intersection(set(valueList2)))
     totaltokens = list(set(valueList1).union(set(valueList2)))
     return float(len(commontokens))  / float(len(totaltokens))
 
+#return the most similar attribute comparing the attribute value of dataset1 with all attribute values in dataset2
 def extractMostSimilarAttribute(attributeName,attributeNames1, attributeNames2):
     similarAttributeName = 0
     mostSimilarity = 0
@@ -52,6 +55,7 @@ def extractMostSimilarAttribute(attributeName,attributeNames1, attributeNames2):
             similarAttributeName = compareAttributeName
     return similarAttributeName
 
+#create clusters of attribute names based on the links between the attribute names in two dataset
 def createTransitiveClosure(clusterLinks1,clusterLinks2):
     clusterLists = []
     #clusterLinks1.update({('year', 1): ('knownFor', 2)})
@@ -60,35 +64,42 @@ def createTransitiveClosure(clusterLinks1,clusterLinks2):
     #print(clusterLinks2)
     for source in clusterLinks1:
         target = clusterLinks1[source]
+        #flag tp switch between the cluster
         switchClusterTarget = 1
         currentCluster = {source,target}
         while True:
             targetPlacedInCluster = 0
+            #switch direction for searching the target
             if switchClusterTarget:
                 target = clusterLinks2[target]
             else:
                 target = clusterLinks1[target]
             for cluster in clusterLists:
                 index = clusterLists.index(cluster)
+                #if attribute name found in existing cluster add the current cluster to it
                 if target in cluster:
                     clusterLists[index] = cluster.union(currentCluster)
                     targetPlacedInCluster = 1
                     break
             if targetPlacedInCluster:
                 break
+            # if attribute name found in current cluster then add the cluster to cluster list
             if target in currentCluster:
                 clusterLists.append(currentCluster)
                 break
+            #if not in any cluster add to current cluster
             currentCluster.add(target)
             switchClusterTarget = not switchClusterTarget
     return clusterLists
 
+#Filter Clusters that contain only 1 attribute name
 def cleanClusterLists(clusterLists):
     for cluster in clusterLists:
         if len(cluster) == 1:
             clusterLists.remove(cluster)
     return clusterLists
 
+#apply token blocking on each clusters
 def createTokenBlocksFromCluster(clusterLists,attributeNames1,attributeNames2):
     attributeNameList = attributeNames1.copy()
     attributeNameList.update(attributeNames2)
@@ -113,13 +124,13 @@ def createTokenBlocksFromCluster(clusterLists,attributeNames1,attributeNames2):
 attributeNames1 = extractAttributeNames(dataset1,1)
 attributeNames2 = extractAttributeNames(dataset2,2)
 
-#get most similar Attribute for datatset1 from dataset2
+#get most similar Attribute for datatset1 from dataset2 and create link
 for attributeName in attributeNames1:
     mostSimilarAttributeName = extractMostSimilarAttribute(attributeName,attributeNames1,attributeNames2)
     if not mostSimilarAttributeName == 0:
         clusterLinks1[attributeName] = mostSimilarAttributeName
 
-#get most similar Attribute for datatset2 from dataset1
+#get most similar Attribute for datatset2 from dataset1 and create link
 for attributeName in attributeNames2:
     mostSimilarAttributeName = extractMostSimilarAttribute(attributeName,attributeNames2,attributeNames1)
     if not mostSimilarAttributeName == 0:
