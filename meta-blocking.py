ea4a2048004655146e6babe220863bf63b6c72cd
antti <antti@data-ml-380-2020-2021>
2021-02-28 14:01:32 +0200

    Add Cardinality Node Pruning to meta blocking methods

--- meta-blocking.py
+++ meta-blocking.py
@@ -1,4 +1,7 @@
+from math import ceil
+from itertools import permutations
 from itertools import product as cartesian_product
+
 
 def block_collection_to_graph(block_collection):
     """
@@ -63,3 +66,68 @@ def weight_edge_pruning(edges, weights):
     average_edge_weight = sum(weights)/len(weights)
     treshold = average_edge_weight
     return [edges[i] if w > treshold else "N/A" for i, w in enumerate(weights)]
+
+def get_neighborhood(node, edges, weights):
+    neighboring_nodes = []
+    neighboring_edges = []
+    neighboring_weights = []
+
+    # Find out the neighboring nodes by scanning the endpoints of each edge
+    for i, edge in enumerate(edges):
+        if edge[0] == node:
+            if edge[1] not in neighboring_nodes:
+                neighboring_nodes.append(edge[1])
+            if edge not in neighboring_edges:
+                neighboring_edges.append(edge)
+                neighboring_weights.append(weights[i])
+        elif edge[1] == node:
+            if edge[0] not in neighboring_nodes:
+                neighboring_nodes.append(edge[0])
+            if edge not in neighboring_edges:
+                neighboring_edges.append(edge)
+                neighboring_weights.append(weights[i])
+
+    # Check if the neighboring nodes have edges between each other
+    possible_edges = permutations(neighboring_nodes)
+    for i, edge in enumerate(edges):
+        if edge in possible_edges and edge not in neighboring_edges:
+            neighboring_edges.append(edge)
+            neighboring_weights.append(weights[i])
+
+    # The central node is also part of the neighborhood
+    neighboring_nodes.append(node)
+
+    return neighboring_nodes, neighboring_edges, neighboring_weights
+
+def cardinality_node_pruning(nodes, edges, weights):
+    pruned_weights = []
+    pruned_directed_edges = []
+    for node in nodes:
+        # This stack keeps the k-nearest entities of each node
+        sorted_stack = []
+        (neighboring_nodes,
+        neighboring_edges,
+        neighboring_weights) = get_neighborhood(node, edges, weights)
+        # This treshold (k) is given in the paper[2]
+        local_cardinality_treshold = ceil(0.1 * len(neighboring_edges))
+
+        # Go through the neighboring edges and keep the k-nearest in the stack
+        for nedges_nweights in zip(neighboring_edges, neighboring_weights):
+            sorted_stack.append(nedges_nweights)
+            if local_cardinality_treshold < len(sorted_stack):
+                sorted_stack.sort(key=lambda item: item[1], reverse=True)
+                sorted_stack.pop()
+
+        # Generate directed edges based on the sorted stack
+        for nedge in neighboring_edges:
+            for stack_item in sorted_stack:
+                if nedge == stack_item[0]:
+                    if node == nedge[1]:
+                        outgoing_edge = nedge[::-1]
+                    else:
+                        outgoing_edge = nedge
+                    if outgoing_edge not in pruned_directed_edges:
+                        pruned_directed_edges.append(outgoing_edge)
+                        pruned_weights.append(stack_item[1])
+
+    return nodes, pruned_directed_edges, pruned_weights
